e54c3e75bb667d4cc52f4b6cebafd867917a25e8
d-saikrishna <dsk.cyanide70@example.com>
2025-07-11 11:54:12 +0530

    Updates

---
+++ _app/immutable/nodes/0.DB8bIHIC.js
@@ -0,0 +1,1 @@
+import"../chunks/CWj6FrbW.js";import{A as n,B as a,C as p}from"../chunks/Dd7LcDy5.js";import{s}from"../chunks/BKHjGoJ9.js";const i=!0,u=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function d(o,r){var e=n(),t=a(e);s(t,()=>r.children),p(o,e)}export{d as component,u as universal};
